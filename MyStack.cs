d9da359fde8716752da1cc35e0f8e5edaca44162
Shatabdi Singh <42743126+Shatabdi2621@users.noreply.github.com>
2022-04-11 14:46:37 +0530

    update main stack

--- MyStack.cs
+++ MyStack.cs
@@ -1,42 +1,90 @@
-using System.Threading.Tasks;
 using Pulumi;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
+using Cdn = Pulumi.AzureNative.Cdn;
+using Resources = Pulumi.AzureNative.Resources;
+using Storage = Pulumi.AzureNative.Storage;
 
-class MyStack : Stack
-{
-    public MyStack()
-    {
-        // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("resourceGroup");
+class MyStack : Stack {
+    public MyStack() {
+        var resourceGroup = new Resources.ResourceGroup("resourceGroup");
 
-        // Create an Azure resource (Storage Account)
-        var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+        var storageAccount = new Storage.StorageAccount("storageaccount", new Storage.StorageAccountArgs
+        {
+            Kind = Storage.Kind.StorageV2,
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new Storage.Inputs.SkuArgs
+            {
+                Name = Storage.SkuName.Standard_LRS,
+            },
+        });
+
+        var staticWebsite = new Storage.StorageAccountStaticWebsite("staticWebsite", new Storage.StorageAccountStaticWebsiteArgs
+        {
+            AccountName = storageAccount.Name,
+            ResourceGroupName = resourceGroup.Name,
+            IndexDocument = "index.html",
+            Error404Document = "404.html",
+        });
+
+        var index_html = new Storage.Blob("index.html", new Storage.BlobArgs
         {
             ResourceGroupName = resourceGroup.Name,
-            Sku = new SkuArgs
-            {
-                Name = SkuName.Standard_LRS
-            },
-            Kind = Kind.StorageV2
+            AccountName = storageAccount.Name,
+            ContainerName = staticWebsite.ContainerName,
+            Source = new FileAsset("./wwwroot/index.html"),
+            ContentType = "text/html",
         });
 
-        // Export the primary key of the Storage Account
-        this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
-            Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
-    }
-
-    [Output]
-    public Output<string> PrimaryStorageKey { get; set; }
-
-    private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
-    {
-        var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
+        var notfound_html = new Storage.Blob("404.html", new Storage.BlobArgs
         {
-            ResourceGroupName = resourceGroupName,
-            AccountName = accountName
+            ResourceGroupName = resourceGroup.Name,
+            AccountName = storageAccount.Name,
+            ContainerName = staticWebsite.ContainerName,
+            Source = new FileAsset("./wwwroot/404.html"),
+            ContentType = "text/html",
         });
-        return accountKeys.Keys[0].Value;
+
+        this.StaticEndpoint = storageAccount.PrimaryEndpoints.Apply(primaryEndpoints => primaryEndpoints.Web);
+
+        var profile = new Cdn.Profile("profile", new Cdn.ProfileArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Location = "global",
+            Sku = new Cdn.Inputs.SkuArgs
+            {
+                Name = Cdn.SkuName.Standard_Microsoft,
+            },
+        });
+
+        var endpointOrigin = storageAccount.PrimaryEndpoints.Apply(pe => pe.Web.Replace("https://", "").Replace("/", ""));
+
+        var endpoint = new Cdn.Endpoint("endpoint", new Cdn.EndpointArgs
+        {
+            EndpointName = storageAccount.Name.Apply(sa => $"cdn-endpnt-{sa}"),
+            IsHttpAllowed = false,
+            IsHttpsAllowed = true,
+            OriginHostHeader = endpointOrigin,
+            Origins =
+            {
+                new Cdn.Inputs.DeepCreatedOriginArgs
+                {
+                    HostName = endpointOrigin,
+                    HttpsPort = 443,
+                    Name = "origin-storage-account",
+                },
+            },
+            ProfileName = profile.Name,
+            QueryStringCachingBehavior = Cdn.QueryStringCachingBehavior.NotSet,
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        // CDN endpoint to the website.
+        // Allow it some time after the deployment to get ready.
+        this.CdnEndpoint = endpoint.HostName.Apply(hostName => $"https://{hostName}");
     }
+
+    [Output("staticEndpoint")]
+    public Output<string> StaticEndpoint { get; set; }
+
+    [Output("cdnEndpoint")]
+    public Output<string> CdnEndpoint { get; set; }
 }
